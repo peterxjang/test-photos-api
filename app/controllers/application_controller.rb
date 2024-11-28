ab7118a3fc835c33b644c36aef61c3ac89f5414f
Peter Jang <peter.x.jang@gmail.com>
2024-11-27 20:27:54 -0600

    Add authentication helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
+
+  def current_user
+    User.find_by(id: cookies.signed[:user_id])
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
